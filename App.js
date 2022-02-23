0a2e00575cf2825b3d7f167c6e9e890af4b01d9d
unknown <rautanokka@gmail.com>
2022-02-23 02:53:07 +0200

    lisätty kaikki

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [text, setText] = useState("")
+  const [language, setLanguage] = useState("")
+
+  const speak = () => {
+    Speech.speak(text, {language: language})
+    setText("")
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{marginBottom: 5}}>Current language: {language === "fi" ? "Finnish" : "English"}</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} placeholder="Text here"/>
+      <Button title="Text to speech" onPress={speak}/>
+      <View style={styles.languages}>
+        <Button title="English" onPress={() => setLanguage("en")}/>
+        <Button title="Finnish" onPress={() => setLanguage("fi")}/>
+      </View>
     </View>
   );
 }
@@ -17,4 +34,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  languages: {
+    flexDirection: "row",
+    paddingTop: 20,
+    width: "50%",
+    justifyContent: "space-between"
+  },
+  input: {
+    width: "50%",
+    marginTop: 10,
+    marginBottom: 10,
+    borderBottomWidth : 1.0
+  }
 });
